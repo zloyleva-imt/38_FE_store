f71bee30d7df8b20e8528fc39443968add7a043e
ZloyLeva <zloyleva@gmail.com>
2019-03-10 19:26:01 +0200

    add Edit product action

--- src/components/CatalogItem/index.js
+++ src/components/CatalogItem/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import axios from'axios';
+
 export default (props) => {
     const {id, amount, description, title, price, image } = props.product;
 
@@ -9,12 +11,38 @@ export default (props) => {
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
-                <p className="">
+                <div className="">
                     <div className="product-price">Price: {price}$</div>
                     <div className="product-amount">Amount: {amount}</div>
-                </p>
-                <a href="#" className="btn btn-primary">Add to cart</a>
+                </div>
+                <button className="btn btn-primary" onClick={()=>{addToCart(id)}}>Add to cart</button>
+                <button className="btn btn-danger" onClick={()=>{EditProduct(id)}}>Edit</button>
             </div>
         </div>
     );
 }
+
+const addToCart = (product_id,user_id=1) => {
+    console.log("addToCart");
+    axios.post(`http://localhost:3001/carts/${user_id}`,{
+        "user_id": user_id,
+        "product_id": product_id,
+        "amount": 1
+    })
+        .then(res => {
+            console.log(res);
+
+        });
+};
+
+const EditProduct = (product_id) => {
+    console.log("addToCart");
+    axios.put(`http://localhost:3001/products/${product_id}`,{
+        "title": "new title",
+        "amount": 1
+    })
+        .then(res => {
+            console.log(res);
+
+        });
+};
