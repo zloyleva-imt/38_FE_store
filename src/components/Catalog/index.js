382a9283efa65c83c21006f0320be8521b577e8a
ZloyLeva <zloyleva@gmail.com>
2019-03-10 18:00:36 +0200

    create CatalogList

--- src/components/Catalog/index.js
+++ src/components/Catalog/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import CatalogList from '../CatalogList';
+
 export default () => {
     return (
-        <main className="App-main">
-            Catalog
+        <main className="App-main container">
+            <div className="row">
+                <div className="col">
+                    <h2 className="h2">Catalog</h2>
+                </div>
+            </div>
+
+            <CatalogList />
+
         </main>
     );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 export default () => {
     return (
-        <header className="App-header">
-            E-store
+        <header className="App-header container">
+            <div className="row">
+                <div className="col">
+                    <h1 className="h1">
+                        E-store
+                    </h1>
+                </div>
+            </div>
         </header>
     );
 }
